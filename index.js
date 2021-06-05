def10be93fae88c0fb92db2db84f5b27b6f7b1b1
Alexey Boklogov <kashira87@mail.ru>
2021-06-05 11:02:00 +0300

    доделал Переключатель цветов

--- index.js
+++ index.js
@@ -3,21 +3,29 @@ import colors from './src/colors.json';
 const startBtnEl = document.querySelector('[data-action="start"]');
 const stopBtnEl = document.querySelector('[data-action="stop"]');
 const bodyEl = document.querySelector('body');
-
 const firstIndex = colors.indexOf(colors[0]);
 const lastIndex = colors.indexOf(colors[colors.length - 1]);
-
 let colorIndex = 0;
+let idSetInterval = 0;
 
-startBtnEl.addEventListener('click', onSwitchColorsClick);
+startBtnEl.addEventListener('click', onStartSwitchColorsClick);
 
-function onSwitchColorsClick() {
-    setInterval(() => {
+stopBtnEl.addEventListener('click', onStopSwitchColorsClick);
+
+function onStartSwitchColorsClick() {
+  idSetInterval = setInterval(() => {
     colorIndex = randomIntegerFromInterval(firstIndex, lastIndex);
     bodyEl.style.backgroundColor = colors[colorIndex];
-    }, 1000);
+  }, 1000);
+
+  startBtnEl.disabled = true;
 };
 
- function randomIntegerFromInterval(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+function onStopSwitchColorsClick() {
+  clearInterval(idSetInterval);
+  startBtnEl.disabled = false;
+};
+
+function randomIntegerFromInterval(min, max) {
+return Math.floor(Math.random() * (max - min + 1) + min);
 };
